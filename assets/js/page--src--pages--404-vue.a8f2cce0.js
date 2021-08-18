3a607fdb99d4685019a6c48ed326f5cdb5466bb7
nathan-louie <nathanhlouie@gmail.com>
2021-08-18 14:07:17 -0400

    Updates

---
+++ assets/js/page--src--pages--404-vue.a8f2cce0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"7l0S":function(t,e,n){"use strict";n.r(e);var l=n("KHd+"),s=Object(l.a)({},(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("Hello, world!")])])}],!1,null,null,null);e.default=s.exports}}]);
