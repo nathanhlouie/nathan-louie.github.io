64dde1691cadc2c69dba64176f67a634346bfbbc
nathan-louie <nathanhlouie@gmail.com>
2021-09-12 20:54:00 -0400

    add new rhythm game

---
+++ content/posts/uwubot.md
@@ -0,0 +1,27 @@
+---
+title: Rhythm Game
+cover_image: ./images/uwubot.jpg
+canonical_url: true
+description: "Experimental Rhythm Game to learn more about Javascript."
+---
+
+# Rhythm Game
+
+I've always loved playing games, so I figured I would try making one myself. I made this rhythm game using Javascript with the Vue.js framework.
+
+Check out the repo [here](https://github.com/nathan-louie/rhythm-game).
+
+# Features
+
+![Playing View](./images/rhythm-game.png)
+
+There are only a certain amount of songs that players can play in this game.
+
+The notes scroll down from the top of the screen, and the player must press the "D", "F", "H", or "J" key corresponding to the column of notes in time.
+
+The player is scored for each game based on the timing of each note press throughout.
+
+# Stack
+
+- This project was created with Javascript and Vue.js
+- Utilized Midi.js and Pixi.js
--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
+  <Layout>
+    <div class="author">
+      <h1 class="author__site-title">
+        Hey! I'm Nathan Louie
+      </h1>
+
+      <p class="author__intro">
+        I'm a high school student trying to learn more about computer science,
+        and I'm passionate about solving intricate problems!
+      </p>
+      <p class="author__intro">
+        - Avid gamer, musician and chess enthusiast
+      </p>
+      <p class="author__intro">
+        - Currently learning about web development
+      </p>
+
+      <g-image src="~/assets/images/firstphoto.png" />
+    </div>
+  </Layout>
 </template>
+
+<script>
+export default {
+  components: {},
+  metaInfo: { title: "404" },
+};
+</script>
+
+<style lang="scss">
+.author {
+  margin: 0 auto;
+  max-width: 770px;
+  text-align: left;
+  padding: calc(var(--space) / 2) 0;
+
+  &__image {
+    border-radius: 100%;
+    width: 90px;
+    height: 90px;
+    margin-bottom: 1em;
+  }
+
+  &__intro {
+    opacity: 0.8;
+  }
+
+  &__site-title {
+    font-size: 1.5em;
+  }
+
+  &__links {
+    margin-top: -0.5em;
+    a {
+      padding-inline: 0 0.5em;
+    }
+  }
+}
+</style>
