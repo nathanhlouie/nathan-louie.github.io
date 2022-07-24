9bad9bd16a33b900888bb8c259961bd4f7da00ad
nathan-louie <nathanhlouie@gmail.com>
2022-07-24 17:53:55 -0400

    move favicon

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,6 +8,18 @@ module.exports = {
     Post: "/:title",
   },
 
+  icon: {
+    favicon: {
+      src: "./src/assets/images/favicon.png",
+      sizes: [16, 32, 96],
+    },
+    touchicon: {
+      src: "./src/assets/images/favicon.png",
+      sizes: [76, 152, 120, 167],
+      precomposed: true,
+    },
+  },
+
   plugins: [
     {
       use: "@gridsome/source-filesystem",
